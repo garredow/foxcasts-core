882b4198181650365dcbb30edb613f66c90f2950
Garrett Downs <choorp@gmail.com>
2019-08-09 22:00:19 -0400

    test: add Mocha and Chai

--- utils/formatPodcast.test.ts
+++ utils/formatPodcast.test.ts
@@ -1,34 +1,35 @@
+import { expect } from 'chai';
 import formatPodcast from './formatPodcast';
 
 describe('formatPodcast', () => {
-  it('should return a properly formatted podcast', () => {
-    const rawPodcast = {
-      collectionId: 1,
-      artistId: 'artistId',
-      collectionName: 'collectionName',
-      artistName: 'artistName',
-      artworkUrl30: 'artworkUrl30',
-      artworkUrl60: 'artworkUrl60',
-      artworkUrl100: 'artworkUrl100',
-      artworkUrl600: 'artworkUrl600',
-      summary: 'summary',
-      description: 'description',
-      feedUrl: 'feedUrl',
-    };
+    it('should return a properly formatted podcast', () => {
+        const rawPodcast = {
+            collectionId: 1,
+            artistId: 'artistId',
+            collectionName: 'collectionName',
+            artistName: 'artistName',
+            artworkUrl30: 'artworkUrl30',
+            artworkUrl60: 'artworkUrl60',
+            artworkUrl100: 'artworkUrl100',
+            artworkUrl600: 'artworkUrl600',
+            summary: 'summary',
+            description: 'description',
+            feedUrl: 'feedUrl'
+        };
 
-    const podcast = formatPodcast(rawPodcast);
+        const podcast = formatPodcast(rawPodcast);
 
-    expect(podcast.id).toEqual(rawPodcast.collectionId);
-    expect(podcast.authorId).toEqual(rawPodcast.artistId);
-    expect(podcast.title).toEqual(rawPodcast.collectionName);
-    expect(podcast.author).toEqual(rawPodcast.artistName);
-    expect(podcast.summary).toEqual(rawPodcast.summary);
-    expect(podcast.description).toEqual(rawPodcast.description);
-    expect(podcast.feedUrl).toEqual(rawPodcast.feedUrl);
+        expect(podcast.id).equal(rawPodcast.collectionId);
+        expect(podcast.authorId).equal(rawPodcast.artistId);
+        expect(podcast.title).equal(rawPodcast.collectionName);
+        expect(podcast.author).equal(rawPodcast.artistName);
+        expect(podcast.summary).equal(rawPodcast.summary);
+        expect(podcast.description).equal(rawPodcast.description);
+        expect(podcast.feedUrl).equal(rawPodcast.feedUrl);
 
-    expect(podcast.cover[30]).toEqual(rawPodcast.artworkUrl30);
-    expect(podcast.cover[60]).toEqual(rawPodcast.artworkUrl60);
-    expect(podcast.cover[100]).toEqual(rawPodcast.artworkUrl100);
-    expect(podcast.cover[600]).toEqual(rawPodcast.artworkUrl600);
-  });
+        expect(podcast.cover[30]).equal(rawPodcast.artworkUrl30);
+        expect(podcast.cover[60]).equal(rawPodcast.artworkUrl60);
+        expect(podcast.cover[100]).equal(rawPodcast.artworkUrl100);
+        expect(podcast.cover[600]).equal(rawPodcast.artworkUrl600);
+    });
 });
