2a1dc7a03590032621535fb6176e06db6cd5624f
Garrett Downs <garredow@outlook.com>
2021-09-16 11:43:30 -0400

    test

--- release.config.js
+++ release.config.js
@@ -5,20 +5,13 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    // [
-    //   '@semantic-release/exec',
-    //   {
-    //     prepareCmd:
-    //       'echo ${lastRelease.version} > old_version && echo ${nextRelease.version} > new_version',
-    //   },
-    // ],
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     successComment: false,
-    //     failComment: false,
-    //   },
-    // ],
-    // '@semantic-release/git',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false,
+      },
+    ],
+    '@semantic-release/git',
   ],
 };
